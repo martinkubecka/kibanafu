b69b7727c7678a8b80326cd9327fc217dd8cd84e
Martin Kubečka <martin.kubecka98@gmail.com>
2022-10-15 12:07:47 +0200

    README update

--- README.md
+++ README.md
@@ -4,3 +4,29 @@
 </p>
 
 ---
+---
+## :notebook_with_decorative_cover: Pre-requisites
+
+
+- Python3.X ([download](https://www.python.org/downloads/release/python-3102/))
+- PyYAML (`pip install pyyaml`)
+- create `config.yml` based on the `example.yml` file inside `.config` directory
+
+---
+## :speech_balloon: Usage
+
+```
+usage: kibanafu.py [-h] [--index NAME] [--field NAME] [--time TIME] [--input FILENAME] [--output FILENAME] [--action ACTION]
+
+Kibanafu parses IP IOCs and builds a query with defined parameters for Kibana.
+
+options:
+  -h, --help         show this help message and exit
+  --index NAME       index name [events/syslog] (default: syslog)
+  --field NAME       field name [source/destination] (default: source)
+  --time TIME        time frame [15m/30m/1h/24h/7d/30d/90d/1y] (default: 7d)
+  --input FILENAME   input file containg IPs (default: ips.txt)
+  --output FILENAME  output file for Kibana query (default: kibana_query.txt)
+  --action ACTION    action to execute [browser/file] (default: browser)
+
+```
--- kibanafu.py
+++ kibanafu.py
@@ -5,6 +5,7 @@ import argparse
 import platform
 import yaml
 
+
 def banner():
     print(r"""
     __    _  __                    ___
@@ -12,7 +13,6 @@ def banner():
   /  '_// // _ \/ _ `// _ \/ _ `// _// // /
  /_/\_\/_//_.__/\_,_//_//_/\_,_//_/  \_,_/
 
--------------------------------------------
     """)
 
 
@@ -33,18 +33,18 @@ def configurate(index, param):
             print("[!] Error occurred: Invalid paramter.")
     else:
         print("[!] Error occurred: Invalid index.")
-    
+
     return query_parameter
 
 
 def open_browser(domain, index, config_indexes, kibana_query, time_param):
-    if index == "events": 
+    if index == "events":
         index_param = config_indexes[0]
         url = "{}/app/discover#/?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-{},to:now))&_a=(columns:!(extra.context,source.ip,source.port,destination.ip,destination.port,classification.taxonomy),filters:!(),{},interval:auto,query:(language:kuery,query:'{}'),sort:!(!('@timestamp',desc),!(time.source,desc)))".format(domain, time_param, index_param, kibana_query)
         webbrowser.open(url, new=2)
     elif index == "syslog":
         index_param = config_indexes[1]
-        url = "{}/app/discover#/?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-{},to:now))&_a=(columns:!(env_name,extra.event.src_ip,extra.event.src_port,extra.event.dest_ip,extra.event.dest_port,extra.event.alert.signature),filters:!(),{},interval:auto,query:(language:kuery,query:'{}'),sort:!(!('@timestamp',desc),!(time.source,desc)))".format(domain, time_param, index_param,kibana_query)
+        url = "{}/app/discover#/?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-{},to:now))&_a=(columns:!(env_name,extra.event.src_ip,extra.event.src_port,extra.event.dest_ip,extra.event.dest_port,extra.event.alert.signature),filters:!(),{},interval:auto,query:(language:kuery,query:'{}'),sort:!(!('@timestamp',desc),!(time.source,desc)))".format(domain, time_param, index_param, kibana_query)
         webbrowser.open(url, new=2)
     else:
         print("[!] Error occurred")
@@ -59,7 +59,7 @@ def parse_ips(filename):
 def build_query(lines, query_parameter):
     query_builder = []
     for line in lines:
-        query = query_parameter  +  line
+        query = query_parameter + line
         query_builder.append(query)
 
     return " OR ".join(query_builder)
@@ -77,14 +77,21 @@ def load_config(filename):
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Kibanafu parses IP IOCs and builds a query with defined parameters for Kibana.')
-    
-    parser.add_argument('--index', metavar="NAME", default="syslog",help='index name [events/syslog] (default: syslog)')
-    parser.add_argument('--field', metavar="NAME", default="source",help='field name [source/destination] (default: source)')
-    parser.add_argument('--time', metavar="TIME", default="7d",help='time frame [15m/30m/1h/24h/7d/30d/90d/1y] (default: 7d)')
-    parser.add_argument('--input', metavar="FILENAME", default="ips.txt",help='input file containg IPs (default: ips.txt)')
-    parser.add_argument('--output', metavar="FILENAME", default="kibana_query.txt", help='output file for Kibana query (default: kibana_query.txt)')
-    parser.add_argument('--action', metavar="ACTION", default="browser", help='action to execute [browser/file] (default: browser)')
+    parser = argparse.ArgumentParser(
+        description='Kibanafu parses IP IOCs and builds a query with defined parameters for Kibana.')
+
+    parser.add_argument('--index', metavar="NAME", default="syslog",
+                        help='index name [events/syslog] (default: syslog)')
+    parser.add_argument('--field', metavar="NAME", default="source",
+                        help='field name [source/destination] (default: source)')
+    parser.add_argument('--time', metavar="TIME", default="7d",
+                        help='time frame [15m/30m/1h/24h/7d/30d/90d/1y] (default: 7d)')
+    parser.add_argument('--input', metavar="FILENAME", default="ips.txt",
+                        help='input file containg IPs (default: ips.txt)')
+    parser.add_argument('--output', metavar="FILENAME", default="kibana_query.txt",
+                        help='output file for Kibana query (default: kibana_query.txt)')
+    parser.add_argument('--action', metavar="ACTION", default="browser",
+                        help='action to execute [browser/file] (default: browser)')
 
     args = parser.parse_args()
     return args
@@ -95,7 +102,8 @@ def main():
 
     machine_platfrom = platform.system()
     if machine_platfrom == "Darwin":    # Mac
-        print("\n[!] Mac is not a supported platform. Please, use Windows or Linux instead.")
+        print(
+            "\n[!] Mac is not a supported platform. Please, use Windows or Linux instead.")
         print("\nExiting program ...\n")
         exit(1)
 
@@ -110,14 +118,14 @@ def main():
     output_file = args.output
 
     config = load_config(".config/config.yml")
-    #config = load_config(".config/example.yml")
+    # config = load_config(".config/example.yml")
     domain = config['app']['domain']
     config_events = config['indexes']['events']
     config_syslog = config['indexes']['syslog']
     config_indexes = []
     config_indexes.append(config_events)
     config_indexes.append(config_syslog)
-    
+
     try:
         print(f"[*] Loading IPs from '{input_file}'")
         lines = parse_ips(input_file)
@@ -131,18 +139,18 @@ def main():
 
     print(f"[*] Building Kibana query with defined options")
     kibana_query = build_query(lines, query_parameter)
-    
+
     if action == "browser":
         time_frame = args.time
         times = {
-            "15m" : "15m",
-            "30m" : "30m",
-            "1h" : "1h",
-            "24h" : "24h/h",
-            "7d" : "7d/d",
-            "30d" : "30d/d",
-            "90d" : "90d/d",
-            "1y" : "1y/d"
+            "15m": "15m",
+            "30m": "30m",
+            "1h": "1h",
+            "24h": "24h/h",
+            "7d": "7d/d",
+            "30d": "30d/d",
+            "90d": "90d/d",
+            "1y": "1y/d"
         }
         time_param = times[time_frame]
         print(f"[*] Opening browser session with built Kibana query")
@@ -152,6 +160,7 @@ def main():
         save_query_to_file(output_file, kibana_query)
 
     print("\n")
+
 
 if __name__ == "__main__":
     main()
